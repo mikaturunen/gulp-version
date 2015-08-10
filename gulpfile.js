2b54a31d8806d1eb81c3d8e5687e56c63e3f84df
Mika Turunen <mika.turunen@ymail.com>
2015-08-10 21:14:45 +0300

    Fix usage of gulp-version.json in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require("gulp");
+var path = require("path");
 var gulpVersion = require("./lib/gulp-version");
 
 gulp.task("default", function() {
     return gulp
         .src([ "./package.json", "bower.json" ], { buffer: false })
-        .pipe(gulpVersion("./gulp-version.json"))
+        .pipe(gulpVersion(path.join(__dirname, "/gulp-version.json")))
         .pipe(gulp.dest("./test"));
 });
